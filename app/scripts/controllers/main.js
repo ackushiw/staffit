a4a76e918c818139a6ce749c6a84d84ef3d0f62c
ackushiw <ackushiw@users.noreply.github.com>
2014-06-01 03:16:05 -0400

    tweaked tables and added time rounded to 15mins

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -142,6 +142,9 @@ angular.module('staffitApp')
     //Real Time Clock
     var tick = function() {            
       $scope.time = new Date();
+      var coeff = 1000 * 60 * 15;
+      var date = new Date();  //or use any other date
+      $scope.xvMinRound = new Date(Math.round(date.getTime() / coeff) * coeff);
       $timeout(tick, 1000);
     };
     $timeout(tick, 1000);
