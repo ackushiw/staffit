77114a894a2ce03056a6f1ae72a41393cfcc5fa6
ackushiw <ackushiw@users.noreply.github.com>
2014-06-03 02:12:05 -0400

    added time to navbar

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('staffitApp')
-  .controller('NavCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('NavCtrl', function ($scope, $timeout) {
+  	var tick = function() {            
+      $scope.time = new Date();
+      var coeff = 1000 * 60 * 15;
+      var date = new Date();  //or use any other date
+      $scope.xvMinRound = new Date(Math.round(date.getTime() / coeff) * coeff);
+      $timeout(tick, 1000);
+    };
+    $timeout(tick, 1000);    
   });
