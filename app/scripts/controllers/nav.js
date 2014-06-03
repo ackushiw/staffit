28f0ede77eafd6b1a9bebc36b9d9d7f6bf3a6432
ackushiw <ackushiw@users.noreply.github.com>
2014-06-03 02:04:53 -0400

    added nav controller

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('staffitApp')
+  .controller('NavCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
