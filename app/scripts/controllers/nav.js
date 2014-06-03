affc9779fa7ce8e7b39650d37930730fd424bdce
ackushiw <ackushiw@users.noreply.github.com>
2014-06-03 16:50:29 -0400

    added momentjs and off canvas sidebar

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -2,6 +2,15 @@
 
 angular.module('staffitApp')
   .controller('NavCtrl', function ($scope, $timeout) {
+  	$scope.sidebarMenu = false;
+
+  	$scope.sidebarToggle = function(sidebar){  		
+  		if (sidebar == false){
+  			$scope.sidebarMenu = true;
+  		} else {
+  			$scope.sidebarMenu = false;
+  		}
+  	};
   	var tick = function() {            
       $scope.time = new Date();
       var coeff = 1000 * 60 * 15;
