382fcc61f4ef650f4a47d31ce0fc46bad1772453
ackushiw <ackushiw@users.noreply.github.com>
2014-06-11 23:51:53 -0400

    stetting up global ctrl as the umbrella authentication controller

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('staffitApp')
-  .controller('GlobalCtrl', function ($scope, USER_ROLES, AuthService) {
+  .controller('GlobalCtrl', function ($scope, syncData, users, USER_ROLES, AuthService) {
+  	$scope.session.fireData = users;
     $scope.currentUser = null;
   	$scope.userRoles = USER_ROLES;
   	$scope.isAuthorized = AuthService.isAuthorized;
