958aafd1f405b6131a50d1cc81fb5a2b79d294e4
ackushiw <ackushiw@users.noreply.github.com>
2014-07-16 18:53:06 -0400

    added mission control view and ctrl

---
+++ app/scripts/controllers/mission.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name staffitApp.controller:MissionCtrl
+ * @description
+ * # MissionCtrl
+ * Controller of the staffitApp
+ */
+angular.module('staffitApp')
+  .controller('MissionCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
