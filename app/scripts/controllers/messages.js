e1abd61a3596932162279ca2a3fc9b6179fb3cca
ackushiw <ackushiw@users.noreply.github.com>
2014-07-20 13:42:13 -0400

    created message directive and controller

---
+++ app/scripts/controllers/messages.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name staffitApp.controller:MessagesCtrl
+ * @description
+ * # MessagesCtrl
+ * Controller of the staffitApp
+ */
+angular.module('staffitApp')
+  .controller('MessagesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/directives/messages.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name staffitApp.directive:messages
+ * @description
+ * # messages
+ */
+angular.module('staffitApp')
+  .directive('messages', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'views/profile-messages.html',
+      controler: 'MessagesCtrl'
+    };
+  });
