ab1118da90be17e33366ea9184df5786ead804b2
ackushiw <ackushiw@users.noreply.github.com>
2014-08-08 19:44:10 -0400

    experimenting with different design for default view

--- app/scripts/controllers/profilepage.js
+++ app/scripts/controllers/profilepage.js
@@ -8,8 +8,52 @@
  * Controller of the staffitApp
  */
 angular.module('staffitApp')
-  .controller('ProfilePageCtrl', function($scope, $stateParams) {
-    var test = $stateParams.id;
-    console.log($stateParams);
-    console.log(test);
+  .controller('ProfilePageCtrl', function($scope, $firebase, FBURL, eventDatabase) {
+    var fireRef = new Firebase(FBURL + '/' + eventDatabase);
+    var sync = $firebase(fireRef);
+    $scope.CalendarCtrl = true;
+    var eventLib = sync.$asArray();
+
+    eventLib.$loaded().then(function(data) {
+
+
+      var calendarSource = [];
+      angular.forEach(data, function(dataChild) {
+
+        console.log(dataChild); // Prints items in order they appear in Firebase.
+        var key = dataChild.$id;
+        console.log(key);
+        dataChild.calendar.url = '#/events/' + key;
+        console.log(dataChild.calendar);
+        if (dataChild.calendar) {
+          this.push(dataChild.calendar);
+        }
+      }, calendarSource);
+      //console.log(calendarSource);
+      $scope.calSource = calendarSource;
+      $scope.eventSources = [{
+        //url: 'https://www.google.com/calendar/feeds/en_gb.usa%23holiday%40group.v.calendar.google.com/public/basic',
+        events: calendarSource
+      }];
+    });
+
+
+    $scope.uiConfig = {
+      calendar: {
+        header: {
+          left: 'month agendaWeek agendaDay',
+          center: 'title',
+          right: 'today prev,next'
+        }
+      }
+    };
+    /*$scope.eventSources = [{
+  //url: 'https://www.google.com/calendar/feeds/en_gb.usa%23holiday%40group.v.calendar.google.com/public/basic',
+  events: calendarSource
+}, {
+  //events: eventLib.$getIndex()
+}];
+*/
+
+
   });
